356a8709bb5cfab50a503bd4b8c01e407bf3fd0a
Pj <47722275+Pj6595@users.noreply.github.com>
2022-02-21 21:09:31 +0100

    Cumpleaños feliz?????? owo

--- hoja3.py
+++ hoja3.py
@@ -1,12 +1,17 @@
 import kbhit
-import pyaudio
+import sounddevice as sd
+import soundfile as sf
 from scipy.io import wavfile
 import numpy as np
 import matplotlib
 from format_tools import *
+from basicGenerators import *
+
+CHUNK = 1024
 
 SRATE = 44100
-SECONDS = 50
+SECONDS = 0.5
+VOLUME = 1.0
 frequency = 50
 
 partitura = [('G', 0.5), ('G', 0.5), ('A', 1), ('G', 1),
@@ -35,9 +40,10 @@ def noteToFreq(note):
         return notes[note.upper()]*2
 
 def generateMusicData(partitura):
-    data = np.zeros(1,0)
+    data = np.empty([1,0])
     for nota, duracion in partitura:
-        np.concatenate(data, osc(noteToFreq(nota), duracion))
+        data = np.append(data, osci(noteToFreq(nota), duracion, VOLUME))
+    return data
 
 
 def osc(f,d):
@@ -69,7 +75,11 @@ def saw(f,d):
 # abrimos wav y recogemos frecMuestreo y array de datos
 # SRATE, data = wavfile.read('piano.wav')
 
-#data = squ(frequency, SECONDS)
+test = np.zeros(shape = [1, 0])
+for t in range(0, 5):
+    test = np.append(test, t)
+
+#data = osci(frequency, SECONDS, VOLUME)
 data = generateMusicData(partitura)
 data = toFloat32(data)
 
@@ -80,43 +90,84 @@ print("Sample format: ",data.dtype)
 print("Num channels: ",len(data.shape))
 print("Len: ",data.shape[0])
 
+# abrimos stream de salida
+stream = sd.OutputStream(
+    samplerate = SRATE,            # frec muestreo 
+    blocksize  = CHUNK,            # tamaño del bloque (muy recomendable unificarlo en todo el programa)
+    channels   = len(data.shape))  # num de canales
+ 
+# arrancamos stream
+stream.start()
 
-# arrancamos pyAudio
-p = pyaudio.PyAudio()
+# En data tenemos el wav completo, ahora procesamos por bloques (chunks)
+# bloque = np.arange(CHUNK,dtype=data.dtype)
+# numBloque = 0
+# kb = kbhit.KBHit()
+# c= ' '
+# while len(bloque>0) and c!= 'q': 
+#     # nuevo bloque
+#     bloque = data[ numBloque*CHUNK : numBloque*CHUNK+CHUNK ]    
 
-CHUNK = 1024
-stream = p.open(format=p.get_format_from_width(getWidthData(data)),
-                channels=len(data.shape),
-                rate=SRATE,
-                frames_per_buffer=CHUNK,
-                output=True)
+#     # pasamos al stream  haciendo conversion de tipo 
+#     stream.write(bloque.astype(data.dtype).tobytes())
+
+#     if kb.kbhit():
+#         c = kb.getch()
+#         if(c == 'F'): 
+#             frequency = frequency + 5
+#             data = osc(frequency, SECONDS)
+#         elif(c == 'f'):
+#             frequency = frequency - 5
+#             data = osc(frequency, SECONDS)
+
+#     numBloque += 1
+#     print('.',end='')
+
+# kb.set_normal_term()        
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
 
 
 # En data tenemos el wav completo, ahora procesamos por bloques (chunks)
-bloque = np.arange(CHUNK,dtype=data.dtype)
+# bloque = np.arange(CHUNK,dtype=data.dtype)
 numBloque = 0
 kb = kbhit.KBHit()
 c= ' '
-while len(bloque>0) and c!= 'q': 
+
+nSamples = CHUNK 
+print('\n\nProcessing chunks: ',end='')
+
+# termina con 'q' o cuando el último bloque ha quedado incompleto (menos de CHUNK samples)
+while c!= 'q' and nSamples==CHUNK: 
+    # numero de samples a procesar: CHUNK si quedan sufucientes y si no, los que queden
+    nSamples = min(CHUNK,data.shape[0] - (numBloque+1)*CHUNK)
+
     # nuevo bloque
-    bloque = data[ numBloque*CHUNK : numBloque*CHUNK+CHUNK ]    
+    bloque = data[numBloque*CHUNK : numBloque*CHUNK+nSamples ]
+    bloque *= VOLUME
 
-    # pasamos al stream  haciendo conversion de tipo 
-    stream.write(bloque.astype(data.dtype).tobytes())
+    # lo pasamos al stream
+    stream.write(bloque) # escribimos al stream
 
+    # modificación de volumen 
     if kb.kbhit():
         c = kb.getch()
         if(c == 'F'): 
             frequency = frequency + 5
             data = osc(frequency, SECONDS)
+            data = toFloat32(data)
         elif(c == 'f'):
             frequency = frequency - 5
             data = osc(frequency, SECONDS)
+            data = toFloat32(data)
+        if (c=='v'): VOLUME= max(0,VOLUME-0.05)
+        elif (c=='V'): VOLUME= min(1,VOLUME+0.05)
+        print("Vol: ",VOLUME)
 
     numBloque += 1
     print('.',end='')
 
-kb.set_normal_term()        
-stream.stop_stream()
-stream.close()
-p.terminate()
+
+print('end')
+stream.stop()
